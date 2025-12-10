02ea6964f2e4c1a18831fd17b0a5920c274a7511
Daniel Gnitetskiy <dgnitetskiy@gmail.com>
2025-12-10 16:53:13 +0300

    add comments

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import math
 
+# Calculate hypotenuse
 def get_hypotenuse(a, b):
     return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 
+# Calculate area
 degf get_area(a, b):
     return a * b
 
 
+# Validate the result
 if __name__ == "__main__":
     print ("Enter digit a:")
     a = int(input())
